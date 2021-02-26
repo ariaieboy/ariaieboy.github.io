cb3b194dd41a7976def0ec319fd40cb705cd334c
AriaieBOY <smr.seddighy@gmail.com>
2021-02-26 16:51:07 +0330

    productionr eady build

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [
+        './**/*.html',
+        './**/.js'
+    ],
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
